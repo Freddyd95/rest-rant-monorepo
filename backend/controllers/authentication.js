5f0d2cbf7bc24dda8fb3e99a5452b44c38c51d2e
Freddyd95 <freddydiaz28@hotmail.com>
2024-06-08 21:09:31 -0700

    Added JWT Secret and added user id to authentication.js page

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require("../models");
 const bcrypt = require('bcrypt');
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -15,21 +16,28 @@ router.post('/', async (req, res) => {
             message: `could not find a user with the provided username and password`
         })
     } else {
-        req.session.userId = user.userId
-        console.log(req)
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
 })
 
 router.get('/profile', async (req, res) => {
     console.log(req.session.userId)
     try {
+        const [authenticationMethod, token] = req.headers.authorization.split('')
+
+        if (authenticationMethod == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            const { id } = result.value
+        
         let user = await User.findOne({
             where: {
-                userId: 1
+                userId: id
             }
         })
         res.json(user)
+    }
     } catch {
         res.json(null)
     }
